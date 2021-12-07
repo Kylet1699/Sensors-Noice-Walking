3a80e41e41bed88554f9f3bdae0d0aba0d6e6570
Kyle Tseng <kyle_tseng@sfu.ca>
2021-12-06 21:08:11 -0800

    added comments for the file

--- walk-speed.py
+++ walk-speed.py
@@ -2,40 +2,35 @@ import numpy as np
 import pandas as pd
 import sys
 from scipy import signal
-from scipy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 
 # Can you calculate walking speed (m/s) from just the accelerometer? 
 # How accurate can you get? 
 # Can you then determine differences between subjects as above?
+'''
+INPUT:
+    arg1: Left leg data csv file with accelerometer data containing column 'time', 'ax'
+    arg2: Right leg data csv file with accelerometer data containig column 'time', 'ax'
 
-# Get data from sys.argv[1]
+OUTPUT:
+    Individual output files with data containing 'time', 'ax', 'butterworth', 'delta_v', 'velocity', 'displacement'
+'''
+
 def get_data(filename):
     data = pd.read_csv(filename)
-    # data.rename(columns = {'time': 'time(ms)'}, inplace = True)
+    data = data[['time', 'ax']]
+    # Reverse +/- because phone is facing the opposite direction when attached to right leg
+    if 'right' in filename:
+        reverse_orientation = lambda x : x * (-1) if x != 0 else x
+        data['ax'] = data['ax'].apply(reverse_orientation)    
     return data
 
-# Clean up the data
-def filter_data(left, right):
-    left = left[['time', 'ax']]
-    right = right[['time', 'ax']]
-    reverse_orientation = lambda x : x * (-1) if x != 0 else x
-    right['ax'] = right['ax'].apply(reverse_orientation)
-    left = left.rename(columns={'time': 'time_left', 'ax' : 'ax_left'})
-    right = right.rename(columns={'time': 'time_right', 'ax' : 'ax_right'})
-    data = left.join(right).fillna(0)
-    return data
-
-def plot(data):
+def plot(data, title):
     plt.figure(figsize=(16,6))
-    plt.title('Left') 
-    plt.plot(data['time_left'], data['ax_left'], 'b.', alpha = 0.5, label = 'Left velocity')
-    plt.plot(data['time_left'], data['butterworth_right'], 'r')
-    plt.show()
-    plt.figure(figsize=(16,6))
-    plt.title('Right')
-    plt.plot(data['time_left'], data['ax_right'], 'b.', alpha = 0.5)
-    plt.plot(data['time_left'], data['butterworth_right'], 'r')
+    plt.title(title) 
+    plt.plot(data['time'], data['ax'], 'b.', alpha = 0.5, label = 'Input')
+    plt.plot(data['time'], data['butterworth'], 'r', label = 'Butterworth')
+    plt.legend()
     plt.show()
 
 def butterworth(data):
@@ -48,66 +43,59 @@ def butterworth(data):
     w = fc/(fs/2)
     b, a = signal.butter(fc, w, btype = 'highpass', analog=False)
     # Filter data 
-    low_passed = signal.filtfilt(b, a, data['ax_left'])
-    data['butterworth_left'] = low_passed
-    low_passed = signal.filtfilt(b, a, data['ax_right'])
-    data['butterworth_right'] = low_passed
+    low_passed = signal.filtfilt(b, a, data['ax'])
+    data['butterworth'] = low_passed
     return data
 
-# def fourier(data):
-#     n = len(data.index) # number of points
-#     Lx = data['time'][-1:0] # time period (ms)
-    
 # Calculate velocity from acceleration
 # Δv = a⋅Δt and Δp = v⋅Δt.
 # get the initial velocity
 # velocity is delta_v + velocity from preious point
 def vel(data):
     data_shifted = data.shift(1)
-    delta_t_left = data['time_left'] - data_shifted['time_left']
-    delta_t_right = data['time_right'] - data_shifted['time_right']
-    print(delta_t_left.mean())
-    print(delta_t_right.mean())
-    print("average left", data['butterworth_left'].mean())
-    print("average_right", data['butterworth_right'].mean())
-    delta_v_left = delta_t_left * data['butterworth_left']
-    delta_v_right = delta_t_right * data['butterworth_right']
-    print(delta_v_left.mean())
-    print(delta_v_right.mean())
-    delta_v_left = pd.DataFrame(delta_v_left, columns=['delta_v_left'])
-    delta_v_right = pd.DataFrame(delta_v_right, columns=['delta_v_right'])
-    data = data.join(delta_v_left)
-    data = data.join(delta_v_right)
-    data['velocity_left'] = 0
-    data['velocity_right'] = 0
-    data.at[0, 'delta_v_left'] = 0
-    data.at[0, 'delta_v_right'] = 0
+    delta_t = data['time'] - data_shifted['time']
+    delta_v = delta_t * data['butterworth']
+
+    # Put delta_v in dataframe to join with main dataframe
+    delta_v = pd.DataFrame(delta_v, columns=['delta_v'])
+    data = data.join(delta_v)
+
+    data['velocity'] = 0
+    data.at[0, 'delta_v'] = 0
+
     for index, row in data.iterrows():
-        data.loc[index, 'velocity_left'] = data.iloc[index]['delta_v_left'] + data.iloc[index-1]['velocity_left']
-        data.loc[index, 'velocity_right'] = data.iloc[index]['delta_v_right'] + data.iloc[index-1]['velocity_right']
-    data['displacement_left'] = data['velocity_left'] * delta_t_left
-    data['displacement_right'] = data['velocity_right'] * delta_t_right
+        data.loc[index, 'velocity'] = data.iloc[index]['delta_v'] + data.iloc[index-1]['velocity']
+
+    # Calculate displacement
+    data['displacement'] = data['velocity'] * delta_t
     return data
 
-def main(input1, input2, output_file):
+def main(input1, input2):
+    # Gets data from input files and filter them
     left = get_data(input1)
     right = get_data(input2)
-    # Clean up the data for later
-    data = filter_data(left, right)
+
     # Performs Butterworth filter on the data
-    data = butterworth(data)
-    # Adds velocity to data
-    data['butterworth_right'] = data['butterworth_right'] + 0.1
-    data = vel(data)
+    left_filtered = butterworth(left)
+    right_filtered = butterworth(right)
 
-    print(data)
-    print("Left distance walked: ", data['displacement_left'].sum())
-    print("Right distance walked: ", data['displacement_right'].sum())
+    # Calculate velocity, displacement
+    # data['butterworth_right'] = data['butterworth_right'] + 0.1
+    left_filtered = vel(left_filtered)
+    right_filtered = vel(right_filtered)
 
+    print(left_filtered)
+    print(right_filtered)
+    print("Left distance walked: ", left_filtered['displacement'].sum())
+    print("Right distance walked: ", right_filtered['displacement'].sum())
+    print("Average left walking speed (m/s): ", left_filtered['velocity'].mean())
+    print("Average right walking speed (m/s): ", right_filtered['velocity'].mean())
 
-    plot(data)
-    data.to_csv(output_file + 'walk-speed')
+    plot(left_filtered, 'Left')
+    plot(right_filtered, 'Right')
+    left_filtered.to_csv(input1 + '-output')
+    right_filtered.to_csv(input1 + '-output')
 
 
 if __name__ == '__main__':
-    main(sys.argv[1], sys.argv[2], sys.argv[3])
+    main(sys.argv[1], sys.argv[2])
